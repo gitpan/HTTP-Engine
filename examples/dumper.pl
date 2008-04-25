b3d8dad0604365ee8f35c8d93479f3f13edec793
Kazuhiro Osawa <yappo+cpan@shibuya.pl>
2008-04-25 18:53:14 +0000

    Import of YAPPO/HTTP-Engine-0.0.1 from CPAN.
    gitpan-cpan-distribution: HTTP-Engine
    gitpan-cpan-version:      0.0.1
    gitpan-cpan-path:         YAPPO/HTTP-Engine-0.0.1.tar.gz
    gitpan-cpan-author:       YAPPO
    gitpan-cpan-maturity:     released

---
+++ examples/dumper.pl
@@ -0,0 +1,26 @@
+use strict;
+use warnings;
+use lib 'lib';
+use Data::Dumper;
+
+use HTTP::Engine;
+
+HTTP::Engine->new( config => 'config.yaml', handle_request => \&handle_request )->run;
+
+sub handle_request {
+    my $c = shift;
+    warn Dumper(\@_);
+    my $req_dump = Dumper($c->req);
+    my $raw = $c->req->raw_body;
+    my $body = <<"...";
+    <form method="post">
+        <input type="text" name="foo" />
+        <input type="submit" />
+    </form>
+    <pre>$raw</pre>
+    <pre>$req_dump</pre>
+...
+
+    $c->res->body($body);
+}
+
