5cd64e1aa577190d2cbb4527c709f20dfa389adb
Kazuhiro Osawa <yappo+cpan@shibuya.pl>
2009-02-12 13:05:12 +0000

    Import of YAPPO/HTTP-Engine-0.1.3 from CPAN.
    gitpan-cpan-distribution: HTTP-Engine
    gitpan-cpan-version:      0.1.3
    gitpan-cpan-path:         YAPPO/HTTP-Engine-0.1.3.tar.gz
    gitpan-cpan-author:       YAPPO
    gitpan-cpan-maturity:     released

--- lib/HTTP/Engine/Interface.pm
+++ lib/HTTP/Engine/Interface.pm
@@ -93,6 +93,11 @@ sub _construct_writer {
             if ($args->{response_line}) {
                 $apply->('ResponseLine');
             }
+            if (my $code = $args->{output_header}) {
+                *{"$writer\::output_header"} = $code;
+            } else {
+                $apply->('OutputHeader');
+            }
             if (my $code = $args->{output_body}) {
                 *{"$writer\::output_body"} = $code;
             } else {
@@ -112,6 +117,9 @@ sub _construct_writer {
 
     for my $before (keys %{ $args->{before} || {} }) {
         $writer->meta->add_before_method_modifier( $before => $args->{before}->{$before} );
+    }
+    for my $around (keys %{ $args->{around} || {} }) {
+        $writer->meta->add_around_method_modifier( $around => $args->{around}->{$around} );
     }
     for my $attribute (keys %{ $args->{attributes} || {} }) {
         Mouse::Meta::Attribute->create( $writer->meta, $attribute,
