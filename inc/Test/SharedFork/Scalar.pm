a88913942bd5e932f77f593dc268db08cee7e784
Kazuhiro Osawa <yappo+cpan@shibuya.pl>
2009-10-26 08:11:44 +0000

    Import of YAPPO/HTTP-Engine-0.03002 from CPAN.
    gitpan-cpan-distribution: HTTP-Engine
    gitpan-cpan-version:      0.03002
    gitpan-cpan-path:         YAPPO/HTTP-Engine-0.03002.tar.gz
    gitpan-cpan-author:       YAPPO
    gitpan-cpan-maturity:     released

---
+++ inc/Test/SharedFork/Scalar.pm
@@ -0,0 +1,23 @@
+#line 1
+package Test::SharedFork::Scalar;
+use strict;
+use warnings;
+use base 'Tie::Scalar';
+
+sub FETCH {
+    my $self = shift;
+    $self->{share}->get('scalar');
+}
+
+sub TIESCALAR {
+    my ($class, $initial, $share) = @_;
+    bless { share => $share }, $class;
+}
+
+sub STORE {
+    my ($self, $val) = @_;
+    my $share = $self->{share};
+    $share->set('scalar' => $val);
+}
+
+1;
---
+++ inc/Test/SharedFork/Store.pm
@@ -0,0 +1,80 @@
+#line 1
+package Test::SharedFork::Store;
+use strict;
+use warnings;
+use Storable ();
+use Fcntl ':seek', ':DEFAULT', ':flock';
+
+sub new {
+    my ($class, $tmpnam) = @_;
+    sysopen my $fh, $tmpnam, O_RDWR|O_CREAT or die $!;
+    bless {fh => $fh, lock => 0}, $class;
+}
+
+sub initialize {
+    my $self = shift;
+
+    truncate $self->{fh}, 0;
+    seek $self->{fh}, 0, SEEK_SET;
+    Storable::nstore_fd(+{
+        array => [],
+        scalar => 0,
+    }, $self->{fh});
+}
+
+sub share { shift->{share} }
+
+sub get {
+    my ($self, $key) = @_;
+
+    my $ret = $self->lock_cb(sub {
+        $self->get_nolock($key);
+    }, LOCK_SH);
+    return $ret;
+}
+
+sub get_nolock {
+    my ($self, $key) = @_;
+    sysseek $self->{fh}, 0, SEEK_SET or die $!;
+    Storable::fd_retrieve($self->{fh})->{$key};
+}
+
+sub set {
+    my ($self, $key, $val) = @_;
+
+    $self->lock_cb(sub {
+        $self->set_nolock($key, $val);
+    }, LOCK_EX);
+}
+
+sub set_nolock {
+    my ($self, $key, $val) = @_;
+
+    sysseek $self->{fh}, 0, SEEK_SET or die $!;
+    my $dat = Storable::fd_retrieve($self->{fh});
+    $dat->{$key} = $val;
+    sysseek $self->{fh}, 0, SEEK_SET or die $!;
+    Storable::nstore_fd($dat => $self->{fh});
+}
+
+sub lock_cb {
+    my ($self, $cb) = @_;
+
+    $self->{lock}++;
+    flock $self->{fh}, LOCK_EX or die $!;
+
+    my $ret = $cb->();
+
+    $self->{lock}--;
+    if ($self->{lock} == 0) {
+        flock $self->{fh}, LOCK_UN or die $!;
+    }
+
+    $ret;
+}
+
+#sub DESTROY {
+#   warn "DESTROY $$ $Test::SharedFork::MODE";
+#}
+
+1;
