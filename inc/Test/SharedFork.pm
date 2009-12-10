ee785c73a56f4efb508c92a76518bf7b91d5de3e
Kazuhiro Osawa <yappo+cpan@shibuya.pl>
2009-12-10 02:52:33 +0000

    Import of YAPPO/HTTP-Engine-0.03003 from CPAN.
    gitpan-cpan-distribution: HTTP-Engine
    gitpan-cpan-version:      0.03003
    gitpan-cpan-path:         YAPPO/HTTP-Engine-0.03003.tar.gz
    gitpan-cpan-author:       YAPPO
    gitpan-cpan-maturity:     released

--- inc/Test/SharedFork.pm
+++ inc/Test/SharedFork.pm
@@ -2,95 +2,45 @@
 package Test::SharedFork;
 use strict;
 use warnings;
-our $VERSION = '0.04';
-use Test::Builder;
+use base 'Test::Builder::Module';
+our $VERSION = '0.09';
+use Test::Builder 0.32; # 0.32 or later is needed
 use Test::SharedFork::Scalar;
 use Test::SharedFork::Array;
 use Test::SharedFork::Store;
-use Storable ();
-use File::Temp ();
-use Fcntl ':flock';
 
-our $TEST;
-my $tmpnam;
 my $STORE;
-our $MODE = 'DEFAULT';
+
 BEGIN {
-    $TEST ||= Test::Builder->new();
-
-    $tmpnam ||= File::Temp::tmpnam();
-
-    my $store = Test::SharedFork::Store->new($tmpnam);
-    $store->lock_cb(sub {
-        $store->initialize();
-    }, LOCK_EX);
-    undef $store;
+    $STORE = Test::SharedFork::Store->new(
+        cb => sub {
+            my $store = shift;
+            tie __PACKAGE__->builder->{Curr_Test}, 'Test::SharedFork::Scalar', 0, $store;
+            tie @{ __PACKAGE__->builder->{Test_Results} }, 'Test::SharedFork::Array', $store;
+        }
+    );
 
     no strict 'refs';
     no warnings 'redefine';
     for my $name (qw/ok skip todo_skip current_test/) {
-        my $cur = *{"Test::Builder::${name}"}{CODE};
+        my $orig = *{"Test::Builder::${name}"}{CODE};
         *{"Test::Builder::${name}"} = sub {
             my @args = @_;
-            if ($STORE) {
-                $STORE->lock_cb(sub {
-                    $cur->(@args);
-                });
-            } else {
-                $cur->(@args);
-            }
+            $STORE->lock_cb(sub {
+                $orig->(@args);
+            });
         };
     };
 }
 
-my @CLEANUPME;
-sub parent {
-    my $store = _setup();
-    $STORE = $store;
-    push @CLEANUPME, $tmpnam;
-    $MODE = 'PARENT';
-}
-
-sub child {
-    # And musuka said: 'ラピュタは滅びぬ！何度でもよみがえるさ！'
-    # (Quote from 'LAPUTA: Castle in he Sky')
-    $TEST->no_ending(1);
-
-    $MODE = 'CHILD';
-    $STORE = _setup();
-}
-
-sub _setup {
-    my $store = Test::SharedFork::Store->new($tmpnam);
-    tie $TEST->{Curr_Test}, 'Test::SharedFork::Scalar', 0, $store;
-    tie @{$TEST->{Test_Results}}, 'Test::SharedFork::Array', $store;
-
-    return $store;
-}
-
-sub fork {
-    my $self = shift;
-
-    my $pid = fork();
-    if ($pid == 0) {
-        child();
-        return $pid;
-    } elsif ($pid > 0) {
-        parent();
-        return $pid;
-    } else {
-        return $pid; # error
-    }
-}
-
-END {
-    undef $STORE;
-    if ($MODE eq 'PARENT') {
-        unlink $_ for @CLEANUPME;
-    }
+{
+    # backward compatibility method
+    sub parent { }
+    sub child  { }
+    sub fork   { fork() }
 }
 
 1;
 __END__
 
-#line 161
+#line 92
